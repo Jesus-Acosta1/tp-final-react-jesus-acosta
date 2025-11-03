513eb4dba16a7ea588be2f9eacb2723a93051299
jesus <jesusaagonzalez@gmail.com>
2025-11-02 23:29:48 -0300

    gh pages

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -8,6 +8,8 @@ function Home() {
     <div className="home">
       <h1>Bienvenido a la Pokedex</h1>
       <h2>gitpages</h2>
+      <h2>gitpages</h2>
+      <h2>gitpages</h2>
       <img className="pokemones" src={pokemones} alt="Pokemones" />
       <div className="home-text">
         <p>
